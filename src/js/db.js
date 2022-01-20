cf5bbe3a7d256ddf0a599dda7ab9f3a6bd1ec897
Pablo Mateos García <>
2022-01-20 20:12:33 +0100

    logic from date and time pickers almost done

--- src/js/db.js
+++ src/js/db.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, setDoc } from "firebase/firestore/lite";
+import { getFirestore, collection, getDocs, doc, setDoc, getDoc } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyChXYwwL0C88rwejI2JcDq9B1QrJjkRI-g",
@@ -49,6 +49,8 @@ const getDatabase = () => {
 const createMenuDict = () => {
   getDatabase();
 
+  checkDateTime(new Date().toDateString(), 11);
+
   return {
     Starter: starter,
     Main: main,
@@ -57,12 +59,80 @@ const createMenuDict = () => {
   };
 };
 
-const addReservation = (date, time, table) =>{
+const times_available = {
+  11: [1, 2, 3, 4, 5, 6], 
+  12: [1, 2, 3, 4, 5, 6], 
+  13: [1, 2, 3, 4, 5, 6], 
+  14: [1, 2, 3, 4, 5, 6], 
+  15: [1, 2, 3, 4, 5, 6], 
+  16: [1, 2, 3, 4, 5, 6], 
+  17: [1, 2, 3, 4, 5, 6], 
+  18: [1, 2, 3, 4, 5, 6], 
+  19: [1, 2, 3, 4, 5, 6],
+  20: [1, 2, 3, 4, 5, 6], 
+  21: [1, 2, 3, 4, 5, 6],
+  22: [1, 2, 3, 4, 5, 6]
+};
+
+const createDate = (date) => {
   setDoc(doc(db, "reservations", date), {
-    times_available: "Los Angeles",
-    times_: "CA",
-    country: "USA"
+    times_available: {
+      11: [1, 2, 3, 4, 5, 6], 
+      12: [1, 2, 3, 4, 5, 6], 
+      13: [1, 2, 3, 4, 5, 6], 
+      14: [1, 2, 3, 4, 5, 6], 
+      15: [1, 2, 3, 4, 5, 6], 
+      16: [1, 2, 3, 4, 5, 6], 
+      17: [1, 2, 3, 4, 5, 6], 
+      18: [1, 2, 3, 4, 5, 6], 
+      19: [1, 2, 3, 4, 5, 6],
+      20: [1, 2, 3, 4, 5, 6], 
+      21: [1, 2, 3, 4, 5, 6],
+      22: [1, 2, 3, 4, 5, 6]
+    },
+    times_booked: [],
   });
+}
+
+const checkTimeValidForDate = (docSnap, time) => {
+  if(docSnap.data().times_available[time] != null){
+    //Hora libre
+    console.log("hora libre")
+    
+  }
+  else{
+    //Hora no disponible
+    console.log("hora no disponible")
+    
+  }
+}
+
+const showToastBottom = () => {
+  // Create toast
+  if (!toastBottom) {
+    toastBottom = $f7.toast.create({
+      text: 'This is default bottom positioned toast',
+      closeTimeout: 2000,
+    });
+  }
+  // Open it
+  toastBottom.open();
+}
+
+const checkDateTime = (date, time) => {
+  const docRef = doc(db, "reservations", date);
+  getDoc(docRef).then(docSnap=>{
+    if (docSnap.exists()) {
+      checkTimeValidForDate(docSnap, time);
+    } else {
+      createDate(date);
+    }
+  })
+
+}
+
+const addReservation = (date, time, table) => {
+  setDoc(doc(db, "reservations", date));
 }
 
 export default createMenuDict;
