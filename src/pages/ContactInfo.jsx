253ef4c124480123b30a10facd4e36aafafa415e
DESKTOP-2OTPOHT\DomAsial <dominik.sasko@asial.co.jp>
2022-01-20 16:18:01 +0100

    Get GPS and location

--- src/pages/ContactInfo.jsx
+++ src/pages/ContactInfo.jsx
@@ -1,6 +1,6 @@
 import "../css/ContactInfo.css";
 import React, { useState } from "react";
-import { Icon, List, ListInput, Page } from "framework7-react";
+import { Icon, f7, List, ListInput, Page } from "framework7-react";
 import "framework7-icons";
 
 import FooterButtons from "../components/FooterButtons";
@@ -8,6 +8,53 @@ const ContactInfo = () => {
   const [customerName, setCustomerName] = useState();
   const [customerEmail, setCustomerEmail] = useState();
   const [customerPhone, setCustomerPhone] = useState();
+  const [customerCity, setCustomerCity] = useState();
+
+  const getGPSPosition = () => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        getCityFromGPSCoord(
+          String(position.coords.latitude),
+          String(position.coords.longitude)
+        );
+
+        // Waiting so the API call finishes before alert - IMPROVE
+        const wait = (timeToDelay) =>
+          new Promise((resolve) => setTimeout(resolve, timeToDelay));
+        await wait(1000);
+
+        f7.dialog.alert(
+          "Nearby city: " + customerCity,
+          "GPS Location Acquired"
+        );
+      },
+      (error) => {
+        f7.dialog.alert("Try again please!", "Acquiring Location Failed");
+        console.log(error);
+      }
+    );
+
+    const getCityFromGPSCoord = (latitude, longitude) => {
+      // Conversion to use the correct URL
+      longitude = longitude.charAt(0) === "-" ? longitude : "+" + longitude;
+      fetch(
+        "http://geodb-free-service.wirefreethought.com/v1/geo/locations/" +
+          latitude +
+          longitude +
+          "/nearbyCities?radius=100&minPopulation=40000&limit=1"
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setCustomerCity(result.data[0].city);
+          },
+          (error) => {
+            f7.dialog.alert("Try again please!", "Acquiring Location Failed");
+            console.log(error);
+          }
+        );
+    };
+  };
 
   return (
     <>
@@ -52,12 +99,7 @@ const ContactInfo = () => {
               <br />
             </h2>
             {/*  Default back icon */}
-            <div
-              className="location_btn"
-              onClick={() =>
-                console.log(customerName, customerEmail, customerPhone)
-              }
-            >
+            <div className="location_btn" onClick={getGPSPosition}>
               <Icon icon="icon-back"></Icon>
             </div>
             {/*  Some custom icon */}
