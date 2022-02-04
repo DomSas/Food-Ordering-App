f7675843c8ca4318c9a9da88f914abffe36d425a
Pablo Mateos García <>
2022-02-04 23:00:20 +0100

    issues fixed

--- src/pages/DateTimePage.jsx
+++ src/pages/DateTimePage.jsx
@@ -51,13 +51,21 @@ const DateTimePage = () => {
   }
 
   //Set the date_time from context if necessary
-  if (!Object.keys(date_time).length === 0) {
-    console.log("date_time")
-    console.log(date_time)
-    console.log("selectedDate")
-    console.log(selectedDate)
-    if (date_time.date != selectedDate) {
-      setSelectedDate(date_time.date);
+  if (!(Object.keys(date_time).length === 0)) {
+    if (
+      new Date(date_time.date).toLocaleDateString("es-ES", {
+        month: "2-digit",
+        year: "numeric",
+        day: "2-digit",
+      }) != selectedDate
+    ) {
+      setSelectedDate(
+        new Date(date_time.date).toLocaleDateString("es-ES", {
+          month: "2-digit",
+          year: "numeric",
+          day: "2-digit",
+        })
+      );
     }
     if (date_time.time != selectedTime) {
       setSelectedTime(date_time.time);
@@ -66,25 +74,21 @@ const DateTimePage = () => {
 
   //UseEffect for checking if the date and time are valid
   useEffect(() => {
-      if (selectedDate && selectedTime && validTime) {
-        const dateSplit = selectedDate.split("/");
-        const date = new Date(
-          dateSplit[1] + "/" + dateSplit[0] + "/" + dateSplit[2]
-          ).toDateString();
-          setSelectedDate(date);
-          checkDateTime(date, selectedTime).then((result) => {
-            if (result) {
-              setDateTime({ date: date, time: selectedTime });
-            } else {
-              let toastCenter = f7.toast.create({
-                text: "Sorry! There are not empty tables in selected date and time. Please choose another one.",
-                closeTimeout: 3000,
-              });
-              toastCenter.open();
-              setValidTime(false);
-            }
+    if (selectedDate && selectedTime && validTime) {
+      const date = new Date(split(selectedDate)).toDateString();
+      checkDateTime(date, selectedTime).then((result) => {
+        if (result) {
+          setDateTime({ date: date, time: selectedTime });
+        } else {
+          let toastCenter = f7.toast.create({
+            text: "Sorry! There are not empty tables in selected date and time. Please choose another one.",
+            closeTimeout: 3000,
           });
+          toastCenter.open();
+          setValidTime(false);
         }
+      });
+    }
   }, [validTime]);
 
   //UseEffect for sending the contact info to the context when the email changes
@@ -111,6 +115,11 @@ const DateTimePage = () => {
     });
   };
 
+  const split = (date) => {
+    let dateSplit = date.split("/");
+    return dateSplit[1] + "/" + dateSplit[0] + "/" + dateSplit[2];
+  };
+
   return (
     <Page name="date-time">
       <NavbarBack />
@@ -128,9 +137,16 @@ const DateTimePage = () => {
             id="dateInput"
             validate
             required
-            value={ selectedDate ? [selectedDate] : [today] }
+            value={selectedDate ? [new Date(split(selectedDate))] : []}
             calendarParams={{ minDate: today }}
-            onInputNotEmpty={(e) => setSelectedDate(e.target.value)}
+            onInputNotEmpty={(e) => {
+              if (
+                selectedDate === undefined ||
+                (selectedDate != undefined && e.target.value != selectedDate)
+              ) {
+                setSelectedDate(e.target.value);
+              }
+            }}
           />
           <ListInput
             className="input_datetime"
@@ -144,7 +160,7 @@ const DateTimePage = () => {
             value={selectedTime ? selectedTime : "11:00"}
             placeholder="Select your time"
             validate
-            onInputNotEmpty={(e) => setSelectedTime(e.target.value)}
+            onInput={(e) => setSelectedTime(e.target.value)}
             onValidate={(isValid) => setValidTime(isValid)}
           />
         </List>
