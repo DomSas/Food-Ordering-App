9b6867ab424c98ec64080267cdba612c7f5879ca
Pablo Mateos García <>
2022-02-10 23:14:10 +0100

    settings button checked

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,59 +1,105 @@
 /* eslint-disable no-undef */
 import {
-  BlockTitle, List, ListItem, Sheet, Toggle,
+  BlockTitle,
+  f7,
+  List,
+  ListItem,
+  Sheet,
+  Toggle,
 } from 'framework7-react';
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Settings.css';
 
 const Settings = () => {
-  const checkCameraAccess = () => {
-    if (cordova.plugins.diagnostic.isCameraAuthorized()) {
-      return true;
+  const [camera, setCamera] = useState(false);
+  const [location, setLocation] = useState(false);
+
+  const checkCameraAccess = async () => {
+    if (camera !== undefined) {
+      let access =
+        await cordova.plugins.diagnostic.getCameraAuthorizationStatus(
+          (status) => {
+            // eslint-disable-next-line default-case
+            switch (status) {
+              case 'not_requested':
+                if (camera !== undefined) {
+                  cordova.plugins.diagnostic.requestCameraAuthorization();
+                }
+                setCamera(false);
+              case 'denied_always':
+                if (camera !== undefined) {
+                  f7.dialog.alert(
+                    'The Settings page for the app will now open. Select "Camera" and set it to "Always" then return to this app via the Home screen',
+                    'Permission denied',
+                    cordova.plugins.diagnostic.switchToSettings,
+                    'Opening Settings page'
+                  );
+                }
+                setCamera(false);
+              default:
+                if (camera !== undefined) {
+                  f7.dialog.alert(
+                    'The Settings page for the app will now open. Select "Camera" and set it to "Never" then return to this app via the Home screen',
+                    'Deny permission through Settings',
+                    cordova.plugins.diagnostic.switchToSettings,
+                    'Deny permission through Settings'
+                  );
+                }
+                setCamera(true);
+            }
+          }
+        );
+      return access;
     }
-    return false;
   };
 
   const checkLocationAccess = () => {
-    if (cordova.plugins.diagnostic.isLocationAuthorized()) {
-      return true;
-    }
-    return false;
-  };
-
-  const cameraChange = () => {
-    if (checkCameraAccess()) {
-      navigator.permissions.revoke({ name: 'camera' });
-    } else {
-      cordova.plugins.diagnostic.requestCameraAuthorization();
-    }
-  };
-
-  const locationChange = () => {
-    if (checkLocationAccess()) {
-      navigator.permissions.revoke({ name: 'location' });
-    } else {
-      cordova.plugins.diagnostic.requestLocationAuthorization();
-    }
+    cordova.plugins.diagnostic.getLocationAuthorizationStatus((status) => {
+      // eslint-disable-next-line default-case
+      switch (status) {
+        case 'not_requested':
+          if (location !== undefined) {
+            cordova.plugins.diagnostic.requestLocationAuthorization();
+            setLocation(false);
+          }
+        case 'denied_always':
+          if (location !== undefined) {
+            f7.dialog.alert(
+              'The Settings page for the app will now open. Select "Location" and set it to "Always" then return to this app via the Home screen',
+              'Permission denied',
+              cordova.plugins.diagnostic.switchToSettings,
+              'Opening Settings page'
+            );
+          }
+          setLocation(false);
+        default:
+          if (location !== undefined) {
+            f7.dialog.alert(
+              'The Settings page for the app will now open. Select "Location" and set it to "Never" then return to this app via the Home screen',
+              'Deny permission through Settings',
+              cordova.plugins.diagnostic.switchToSettings,
+              'Opening Settings page'
+            );
+          }
+          setLocation(true);
+      }
+    });
   };
 
   return (
-    <Sheet swipeToClose closeByOutsideClick className="settings-sheet">
+    <Sheet swipeToClose closeByOutsideClick className='settings-sheet'>
       <BlockTitle>Settings</BlockTitle>
       <List simpleList>
         <ListItem>
           <span>Allow camera access</span>
-          <Toggle
-            onChange={cameraChange}
-            checked={{ checkCameraAccess }}
-            color="gray"
-          />
+          <Toggle onChange={checkCameraAccess} checked={camera} color='gray' />
         </ListItem>
         <ListItem>
           <span>Allow location access</span>
           <Toggle
-            onChange={locationChange}
-            checked={{ checkLocationAccess }}
-            color="gray"
+            onChange={checkLocationAccess}
+            checked={location}
+            color='gray'
           />
         </ListItem>
       </List>
