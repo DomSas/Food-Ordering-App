06ea9b8966b422707edfa38750506d3e0617cd16
yong <yong@asial.co.jp>
2022-02-16 15:59:02 +0900

    check camera and location value when component mounted

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -7,18 +7,79 @@ import {
   Sheet,
   Toggle,
 } from 'framework7-react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/Settings.css';
 
 const Settings = () => {
   const [camera, setCamera] = useState(false);
   const [location, setLocation] = useState(false);
 
-  const checkCameraAccess = async () => {
+  const isPermissionGranted = (status) => {
+    if (status && (status === cordova.plugins.diagnostic.permissionStatus.GRANTED || status === cordova.plugins.diagnostic.permissionStatus.GRANTED_WHEN_IN_USE)) {
+        return true;
+    }
+    return false;
+  };
+
+
+  useEffect(() => {
+      // Check Camera permission
+      cordova.plugins.diagnostic.getCameraAuthorizationStatus({
+        successCallback: function(status){
+            if (isPermissionGranted(status)) {
+                setCamera(true);
+            } else {
+                setCamera(false);
+            }
+        },
+        errorCallback: function(error){
+            console.log(error);
+        },
+        externalStorage: false
+      });
+      // Check Location permission
+        cordova.plugins.diagnostic.getLocationAuthorizationStatus(function(status){
+            if (isPermissionGranted(status)) {
+                setLocation(true);
+            } else {
+                setLocation(false);
+            }
+        }, function(error){
+            console.error(error);
+        });
+  });
+  
+
+  const checkCameraAccess = () => {
+    cordova.plugins.diagnostic.requestCameraAuthorization({
+        successCallback: function(status){
+            const isGranted = isPermissionGranted(status);
+            const message = "Authorization request for camera use was " + (isGranted ? "granted" : "denied") + ". If you would like to change, change it from the phone setting.";
+            f7.dialog.alert(message);
+        },
+        errorCallback: function(error){
+            f7.dialog.alert(error);
+        },
+        externalStorage: false
+    });
+  };
+
+  const checkLocationAccess = () => {
+    cordova.plugins.diagnostic.requestLocationAuthorization(function(status){
+        const isGranted = isPermissionGranted(status);
+        const message = "Authorization request for location use was " + (isGranted ? "granted" : "denied") + ". If you would like to change, change it from the phone setting.";
+        f7.dialog.alert(message);
+    }, function(error){
+        console.error(error);
+    }, cordova.plugins.diagnostic.locationAuthorizationMode.ALWAYS);
+  };
+
+  const checkCameraAccess2 = async () => {
     if (camera !== undefined) {
       let access =
         await cordova.plugins.diagnostic.getCameraAuthorizationStatus(
           (status) => {
+            console.log(status);
             // eslint-disable-next-line default-case
             switch (status) {
               case 'not_requested':
@@ -53,7 +114,7 @@ const Settings = () => {
     }
   };
 
-  const checkLocationAccess = () => {
+  const checkLocationAccess2 = () => {
     cordova.plugins.diagnostic.getLocationAuthorizationStatus((status) => {
       // eslint-disable-next-line default-case
       switch (status) {
